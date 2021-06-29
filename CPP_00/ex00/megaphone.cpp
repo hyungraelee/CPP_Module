8fd7a30a7140160996e09ab77845b3915448200b
hyungraelee <timgg12345@gmail.com>
2021-06-29 21:39:22 +0900

    make megaphone

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// #include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[j]; j++)
+				argv[i][j] = ::toupper(argv[i][j]);
+			std::cout << argv[i];
+			if (argv[i + 1])
+				std::cout << ' ';
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
