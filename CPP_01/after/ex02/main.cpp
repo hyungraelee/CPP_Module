0e26a54a4d97cf1b71673986d9fb173d3b40a97a
hyungraelee <timgg12345@gmail.com>
2021-07-06 20:35:19 +0900

    make ex02

---
+++ CPP_01/after/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int	main()
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "Address in memory of the string : " << &str << std::endl;
+	std::cout << "Address of the string by using stringPTR : " << stringPTR << std::endl;
+	std::cout << "Address of the string by using stringREF : " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Display the string using the pointer : " << *stringPTR << std::endl;
+	std::cout << "Display the string using the reference : " << stringREF << std::endl;
+	return (0);
+}
