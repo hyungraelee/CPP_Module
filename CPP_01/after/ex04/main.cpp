e7006b53b7156e06f364b7a2813442b031432e4d
hyungraelee <timgg12345@gmail.com>
2021-07-07 17:12:43 +0900

    ex04 done

--- CPP_01/after/ex04/main.cpp
+++ CPP_01/after/ex04/main.cpp
@@ -4,29 +4,52 @@
 int main(int argc, char **argv)
 {
 	if (argc != 4)
-		return (0);
+	{
+		std::cout << "Error : argument." << std::endl;
+		return (1);
+	}
 	std::string filename = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	std::ifstream fin(filename.data());
-	std::ofstream fout(filename.append(".replace").data());
 	std::string	str;
+	int	start = 0;
+	int	index = 0;
+	std::string	newline;
 
 	if (fin.is_open())
 	{
+		std::ofstream fout(filename.append(".replace").data());
+		if (!fout.is_open())
+		{
+			fin.close();
+			std::cout << "Error : file open." << std::endl;
+			return (1);
+		}
 		while (!fin.eof())
 		{
 			getline(fin, str);
-			if (str == s1)
-				fout << s2;
-			else
-				fout << str;
+			newline = str;
+			start = 0;
+			index = 0;
+			while ((index = str.find(s1, start)) != -1)
+			{
+				newline.erase(index, s1.length());
+				newline.insert(index, s2);
+				start = index + s1.length();
+			}
+			fout << newline;
 			if (fin.eof())
 				break ;
 			fout << std::endl;
 		}
+		fout.close();
+		fin.close();
 	}
-	fin.close();
-	fout.close();
+	else
+	{
+		std::cout << "File " << filename << " dose not exist." << std::endl;
+		return (1);
+	}
 	return (0);
 }
