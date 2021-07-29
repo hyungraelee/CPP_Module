5dbb156e91ab5dbb7d831ba81b117c037c994ed4
hyungraelee <timgg12345@gmail.com>
2021-07-29 14:47:16 +0900

    done ex00

---
+++ CPP_06/ex00/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ScalarConverter.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		std::cout << "Error : argument." << std::endl;
+		return (-1);
+	}
+	ScalarConverter converter(argv[1]);
+	std::cout << converter << std::endl;
+	return (0);
+}
