88f23955a2cd1472e555225e36d396849c66f05b
hyungraelee <timgg12345@gmail.com>
2021-08-19 15:29:41 +0900

    update checklist code

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -13,6 +13,28 @@ void plusOne(T &a)
 	a += 1;
 }
 
+// ---------------------------------------------------------------
+// ********* for checklist *********
+
+// class Awesome
+// {
+// public:
+// 	Awesome( void ) : _n( 42 ) {}
+// 	int get( void ) const { return this->_n; }
+// private:
+// 	int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+// template< class T >
+// void print( T & x )
+// {
+// 	std::cout << x << std::endl;
+// 	return;
+// }
+// ---------------------------------------------------------------
+
 template <class T>
 void iter(T *arrPtr, unsigned int len, void (*func)(T &a))
 {
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -3,15 +3,16 @@
 
 int main()
 {
-	int a[] = {0, 1, 2, 3, 4};
-
 	std::cout << "-----------------------------------" << std::endl;
+
 	std::cout << "Test type INT" << std::endl;
+	int a[] = {0, 1, 2, 3, 4};
 	::iter(a, 5, ::print<int>);
 	std::cout << std::endl;
 	::iter(a, 5, ::plusOne<int>);
 	::iter(a, 5, ::print<int>);
 	std::cout << std::endl;
+
 	std::cout << "-----------------------------------" << std::endl;
 
 	std::cout << "Test type FLOAT" << std::endl;
@@ -21,7 +22,9 @@ int main()
 	::iter(b, 5, ::plusOne<float>);
 	::iter(b, 5, ::print<float>);
 	std::cout << std::endl;
+
 	std::cout << "-----------------------------------" << std::endl;
+
 	std::cout << "Test type CHAR" << std::endl;
 	char c[] = {'a', 'b', 'c', 'd', 'e'};
 	::iter(c, 5, ::print<char>);
@@ -29,7 +32,23 @@ int main()
 	::iter(c, 5, ::plusOne<char>);
 	::iter(c, 5, ::print<char>);
 	std::cout << std::endl;
+
 	std::cout << "-----------------------------------" << std::endl;
 
 	return (0);
 }
+
+// ---------------------------------------------------------------
+// ********* for checklist *********
+
+// int main()
+// {
+// 	int tab[] = { 0, 1, 2, 3, 4 };
+// 	Awesome tab2[5];
+
+// 	::iter( tab, 5, ::print<int>);
+// 	::iter( tab2, 5, ::print);
+
+// 	return 0;
+// }
+// ---------------------------------------------------------------
